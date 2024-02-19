20b63ad535954643002f705469b72a277f8104ec
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-19 18:24:41 -0500

    add filter sort pagination on server

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -24,6 +24,7 @@ const getAllJobs = async (req, res) => {
     queryObject.jobType = jobType;
   }
 
+  console.log(req.query);
   //so we can chain more logic
   let result = JobModel.find(queryObject);
 
@@ -41,8 +42,21 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position');
   }
 
+  //chain pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res.status(StatusCodes.OK).json({ jobs });
+
+  //total jobs based on query, not users total job
+  const totalJobs = await JobModel.countDocuments(queryObject);
+  //calculate num of pages for that query, and round up
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const getJob = async (req, res) => {
